c383087219cad0219118ecea09b4814d031eb471
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-09-11 15:57:25 -0700

    update task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,6 +5,8 @@ this program display a message depending of the number of args
 const cpt = process.argv.length;
 if (cpt === 2) {
   console.log('No argument');
-} else {
+} else if (cpt === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
