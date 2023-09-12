827027c75003b582c3d3585aef2068535cac2282
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-09-11 17:21:54 -0700

    add task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+this program  that prints My number: <first argument converted in integer>
+*/
+const multi_languages = {
+    first: "C is fun",
+    second: "Python is cool",
+    third: "JavaScript is amazing",
+};
+
+for (const key in multi_languages){
+    console.log(multi_languages[key]);
+}
