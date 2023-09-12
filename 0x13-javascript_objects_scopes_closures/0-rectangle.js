0418efa40b1c688014757815394c12c318e21f40
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-09-12 12:48:17 -0700

    add readme.md file and task 0

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+empty class Rectangle that defines a rectangle
+*/
+
+class Rectangle {
+  constructor (height, width) {
+    this.height = height;
+    this.width = width;
+  }
+}
