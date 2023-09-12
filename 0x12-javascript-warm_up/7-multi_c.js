855a812034acf5d44e95e1020de3b027914b4b91
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-09-11 18:13:49 -0700

    update task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,10 @@
 /*
 this program  that prints x times “C is fun”
 */
-const cpt = process.argv[2];
-if(!process.argv[2]){
-    console.log('Missing number of occurrences')
-}else{
-let i = 0 ;
-while(i!=cpt){
-    console.log('C is fun');
-    i++;
-}}
+const cpt = parseInt(process.argv[2]);
+if (!cpt) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < cpt; i++) {
+  console.log('C is fun');
+}
