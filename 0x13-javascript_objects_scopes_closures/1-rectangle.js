0ad0cd8cdcdc73553597f5e016748294177999c7
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-09-12 13:39:48 -0700

    add task1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+class Rectangle that defines a rectangle
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    this.height = h;
+    this.width = w;
+  }
+}
+module.exports = Rectangle;
